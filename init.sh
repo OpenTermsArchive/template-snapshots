113cfd93d2c00280272b17cd5cbe1fb50cb7fdfc
Martin Ratinaud <martinratinaud@gmail.com>
2022-04-13 10:25:06 +0400

    Rename original README

--- init.sh
+++ init.sh
@@ -17,13 +17,13 @@ fi
 echo "Replacing \${instanceName} and \${instanceMaintainer} by "$INSTANCE_NAME" in files..."
 # Use intermediate backup files (`-i`) with a weird syntax due to lack of portable 'no backup' option. See https://stackoverflow.com/q/5694228/594053.
 # Credit to https://github.com/openfisca/country-template/blob/master/bootstrap.sh
-sed -i.template "s|\${instanceName}|$INSTANCE_NAME|g" README-original.md
-sed -i.template "s|\${instanceMaintainer}|$INSTANCE_MAINTAINER|g" README-original.md
+sed -i.template "s|\${instanceName}|$INSTANCE_NAME|g" README.template.md
+sed -i.template "s|\${instanceMaintainer}|$INSTANCE_MAINTAINER|g" README.template.md
 find . -name "*.template" -type f -delete
 
 echo "Replacing README"
 rm README.md
-mv README-original.md README.md
+mv README.template.md README.md
 
 echo "🎉 You're all done, congratulations"
 rm init.sh
